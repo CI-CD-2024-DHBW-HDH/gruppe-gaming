1015664e272e8598f1b344841e98fdbde7e139fd
Noah <hacksup47@gmail.com>
2024-02-26 10:55:37 +0100

    bugfix of move order

--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -6,18 +6,7 @@ import { randomMove, winningMove } from "./bot";
 // or plays the center field if it can
 // otherwise it plays a random move
 
-export function mediumMove(board: Field[], own: Field): number {
-  if (winningMove(board, own) != -1) {
-    return winningMove(board, own)
-  }
-
-  return pettyMove(board, own)
-}
-
-// this bot just tries to block a win
-// otherwise it plays a random move
-export function pettyMove(board: Field[], own: Field): number {
-  var blanks = getBlanks(board)
+function getPettyMove(board: Field[], own: Field): number {
   var enemy
   if (own == Field.PLAYER1) {
     enemy = Field.PLAYER2
@@ -29,9 +18,36 @@ export function pettyMove(board: Field[], own: Field): number {
   if (winningMove(board, enemy) != -1) {
     return winningMove(board, enemy)
   }
+
+  return -1
+}
+
+export function mediumMove(board: Field[], own: Field): number {
+  var blanks = getBlanks(board)
+  if (winningMove(board, own) != -1) {
+    return winningMove(board, own)
+  }
+
+  if (getPettyMove(board, own) != -1) {
+    return getPettyMove(board, own)
+  }
+  
   if (board[4] == Field.EMPTY) {
     return 4
   }
 
   return blanks[randomMove(blanks.length)]
+}
+
+// this bot just tries to block a win
+// otherwise it plays a random move
+export function pettyMove(board: Field[], own: Field): number {
+  var blanks = getBlanks(board)
+
+  if (getPettyMove(board, own) != -1) {
+    return getPettyMove(board, own)
+  }
+  else {
+    return blanks[randomMove(blanks.length)]
+  }
 }
