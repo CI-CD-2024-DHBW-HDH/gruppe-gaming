9cdbb6acc53ad5ad5c40e59c7de35870280fac0d
Simon <berndts.tin21@student.dhbw-heidenheim.de>
2024-02-26 13:14:32 +0100

    jest config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: [
+        '**/*.test.ts',
+    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config
