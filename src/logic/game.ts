9e1ad3f416b4d19f6428aad416e50d9b9971c24f
tinvol <tinvol@gmail.com>
2024-02-26 10:22:36 +0100

    added won method

--- src/logic/game.ts
+++ src/logic/game.ts
@@ -102,6 +102,37 @@ export function isFull(board: Field[]): boolean {
 }
 
 export function won(board: Field[]): Field {
+  //check horizontal
+  for(let i=0; i<board.length; i += 3){
+    if(board[i] != Field.EMPTY){
+        if(board[i] === board[i+1] && board[i] === board[i+2]){
+          return board[i];
+        }
+    }
+  }
+
+  //check vertical
+  for(let i=0; i<board.length / 3; i ++){
+    if(board[i] != Field.EMPTY){
+        if(board[i] === board[i+3] && board[i] === board[i+6]){
+          return board[i];
+        }
+    }
+  }
+
+  //check diagonal
+  if(board[0] != Field.EMPTY){
+      if(board[0] === board[4] && board[0] === board[8]){
+          return board[0];
+      }
+  }
+
+  if(board[2] != Field.EMPTY){
+    if(board[2] === board[4] && board[2] === board[6]){
+        return board[2];
+    }
+  }
+
   return Field.EMPTY
 }
 
