690b20e1fdb58cfa9716d3c60c41be24b666434c
larsjaeger <lars@jgr.one>
2024-02-26 10:39:05 +0100

    fix easyMove

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -4,17 +4,17 @@ import { randomMove, winningMove } from "./bot";
 // the easy bot plays a winning move, if it can
 // otherwise, it plays a random move
 export function easyMove(board: Field[], own: Field): number {
-  move: number = winningMove(board, own)
-  if (move === -1) {
+  let move = winningMove(board, own);
+  if (move !== -1) {
     return move;
   } else {
-    freeFields: Integer[] = []
-    for (i = 0; i<board.length; i++){
+    let freeFields = [];
+    for (let i = 0; i<board.length; i++){
       if (board[i] === Field.EMPTY) {
         freeFields.push(i);
       }
     }
-    const rndInt = Math.floor(Math.random() * freeFields.length)}
+    const rndInt = Math.floor(Math.random() * freeFields.length)
     return freeFields[rndInt]
   }
 }
